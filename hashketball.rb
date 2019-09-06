9e2c74e0770385655613c0a259d2d4f0cd5bf14f
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 01:27:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,4 +125,9 @@ def shoe_size(desired_player_name)
   end
 end
 
-def team_colors()
+def team_colors(team_name)
+  game_hash.each do |place, team|
+    return game_hash[place][:colors] if team[:team_name] == team_name
+  end
+end
+def 
