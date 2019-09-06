ab1ed32d89de81c76e783066578b092ec5ae5683
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 01:35:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -150,4 +150,20 @@ def player_numbers(team_name)
   numba
 end
 
-def player_stats
+def player_stats(desired_player_name)
+  new_hash = {}
+  game_hash.collect do |place, team|
+    team.each do |attribute, _data|
+      next unless attribute == :players
+
+      game_hash[place][attribute].each do |player|
+        next unless player[:player_name] == desired_player_name
+
+        new_hash = player.delete_if do |keys, _values|
+          keys == :player_name
+        end
+      end
+    end
+  end
+  new_hash
+end
