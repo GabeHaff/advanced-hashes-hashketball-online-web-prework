e91ef013bea6c549fceb2f57722bce5990299717
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 00:54:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,6 @@
 require "pry"
 def game_hash 
-  new_hash = {:home =>{team_name: "" , colors: =>[], players: =>[{}]},:away=>{:team_name, :colors, :players}}
+new_hash = {:home =>{team_name: "" , colors: =>[], players: =>[{}]},:away=>{team_name:"", colors:[], players:[{}]}}
 end 
 
 
