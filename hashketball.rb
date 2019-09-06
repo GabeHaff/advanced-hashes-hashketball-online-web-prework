9c13e8affd5fa85919a0788dbe6603d927021f5d
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 01:16:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -101,4 +101,14 @@ def game_hash
             ] }
   }
 end
-def 
+def num_points_scored(sought_player_name)
+  game_hash.each do |_place, team|
+    team.each do |attribute, data|
+      next unless attribute == :players
+
+      data.each do |player|
+        return player[:points] if player[:player_name] == sought_player_name
+      end
+    end
+  end
+end
