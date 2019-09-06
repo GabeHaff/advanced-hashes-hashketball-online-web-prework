6ed55418d091018cb814200303c533ed2cbcae58
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 01:26:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -113,4 +113,14 @@ def num_points_scored(desired_player_name)
   end
 end
 
-def 
+def shoe_size(desired_player_name)
+  game_hash.each do |_place, team|
+    team.each do |attribute, data|
+      next unless attribute == :players
+
+      data.each do |player|
+        return player[:shoe] if player[:player_name] == desired_player_name
+      end
+    end
+  end
+end
