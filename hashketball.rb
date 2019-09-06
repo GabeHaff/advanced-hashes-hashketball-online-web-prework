51a048ac57b56ddd92e75d4f643a19730ac75adb
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 00:59:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,8 @@
 require "pry"
-def game_hash 
-new_hash = {:home =>{team_name: "Charlotte Hornets" , colors: =>%w["Turquoise"], players: =>[{}]},:away=>{team_name:"", colors:[], players:[{}]}}
+def game_hash  
+new_hash = {:home =>{team_name: "Charlotte Hornets" , colors: =>%w["Turquoise Purple"], players: =>[{}]},:away=>{team_name:"", colors:[], players:[{}]}}
 end 
-
+ 
 
 
 
