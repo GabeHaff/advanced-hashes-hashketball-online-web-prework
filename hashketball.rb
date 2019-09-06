00e921a7a4353ed6a5216f227a1e2332b45b3b4d
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 01:46:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,4 +181,35 @@ def big_shoe_rebounds
   end
 numba_rebounds 
 end 
-def
+def iterate_through_players_for(name, statistic)
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      return player[statistic] if player[:player_name] == name
+    end
+  end
+end
+
+def player_with_most_of(statistic)
+  player_name = nil
+  amount_of_stat = 0
+
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      if player[statistic].is_a? String
+        if player[statistic].length > amount_of_stat
+          amount_of_stat = player[statistic].length
+          player_name = player[:player_name]
+        end
+      elsif player[statistic] > amount_of_stat
+        amount_of_stat = player[statistic]
+        player_name = player[:player_name]
+      end
+    end
+  end
+
+  player_name
+end
+
+def most_points_scored
+  player_with_most_of(:points)
+end
